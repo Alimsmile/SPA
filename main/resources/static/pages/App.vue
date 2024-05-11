8253e1ceaf358a1a7e7ec027fd3540df6d2615c1
Алимов Инсур Эмирович <aie@wms.eme.ru>
2024-05-11 12:04:43 +0300

    Описание основных данных (лифты, очередь вызовов, этажи), реализация макета и стилизация

--- main/resources/static/pages/App.vue
+++ main/resources/static/pages/App.vue
@@ -9,7 +9,25 @@ export default {
 
   data() {
     return {
-
+      // Лифты
+      elevators: [
+        {
+          status: 0,   // состояние
+          position: 1, // положение
+        },
+        {
+          status: 0,   // состояние
+          position: 3, // положение
+        }
+      ],
+      /* Очередь вызовов
+        {
+          floorNum: 0, // номер этажа
+        }
+       */
+      listChallenges: [],
+      // Этажи
+      floors: [1, 2, 3, 4, 5],
     }
   },
 
@@ -25,7 +43,6 @@ export default {
 
 
   methods: {
-
   },
 
 
@@ -34,11 +51,74 @@ export default {
 
 
 <template>
-  <div class="spa">Hello</div>
+  <div class="spa">
+
+
+    <!-- Шахты -->
+    <div class="mine" v-for="mine in elevators" :mine="mine" :style="{'height': 100 * floors.length + 'px'}">
+      <div class="elevator" :style="{'top': (floors.length - mine.position) * 100  + 'px'}">{{mine.position}}</div>
+    </div>
+
+
+    <!-- Этажи -->
+    <div class="floors" :style="{'height': 100 * floors.length + 'px'}">
+      <div class="floor" v-for="floor in floors" :floor="floor">
+        {{floors.length - floor + 1}}
+        <button>⦿</button>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 
 <style>
+.spa {
+  display: flex;
+  padding: 10px;
+  border-top: 5px solid #373736;
+  border-bottom: 5px solid #373736;
 
+
+  .mine {
+    width: 100px;
+    margin-right: 10px;
+    border-left: 3px solid #d9dad9;
+    border-right: 3px solid #d9dad9;
+
+    .elevator {
+      width: 100px;
+      height: 100px;
+      background-color: #00fdfe;
+      position: relative;
+    }
+
+  }
+
+
+  .floors {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    .floor {
+      display: flex;
+      flex-direction: column;
+      height: 100px;
+      button {
+        margin-top: 10px;
+        padding: 3px;
+        background-color: white;
+        border: 1px solid #02969e;
+        border-radius: 5px;
+        color: #02969e;
+      }
+    }
+
+  }
+
+
+}
 </style>
 
